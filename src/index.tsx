33f083c5ee688fbdad1bfc8bf08551b7f11cbf9c
artmadiar <artmadiar@gmail.com>
2022-03-31 20:38:51 +0200

    react refactoring: createRoot in the new v18 way

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import App from './components/App';
 
 const queryClient = new QueryClient();
+const container = document.getElementById('root')!;
+const root = ReactDOMClient.createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ReactQueryDevtools initialIsOpen />
+  </QueryClientProvider>
+  </React.StrictMode>
 );
