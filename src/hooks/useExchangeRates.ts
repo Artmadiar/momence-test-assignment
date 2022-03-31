77a69ad1b2050be0775076bd953bc340e3e51f7e
artmadiar <artmadiar@gmail.com>
2022-03-31 21:12:13 +0200

    implement the parsing of exchange rates;

--- src/hooks/useExchangeRates.ts
+++ src/hooks/useExchangeRates.ts
@@ -1,13 +1,40 @@
 import { useQuery, UseQueryResult } from 'react-query';
+import { IExchangeRate } from '../types';
 
-export default function useExchangeRates(): UseQueryResult<string> {
+/**
+ * Server can't respond JSON
+ * So, we parse the raw text
+ */
+function parseResponse(text: string): IExchangeRate[] {
+  const rows = text.split('\n');
+
+  // remove unnecessary header and table header
+  rows.shift();
+  rows.shift();
+
+  // remove unnecessary last empty string
+  rows.shift();
+
+  const rates: IExchangeRate[] = rows.map((row: string) => {
+    // parse row: země|měna|množství|kód|kurz
+    const parsedRow = row.split('|');
+
+    return {
+      country: parsedRow[0],
+      currency: parsedRow[1],
+      amount: Number.parseInt(parsedRow[2], 10),
+      code: parsedRow[3],
+      rate: Number.parseInt(parsedRow[4], 10),
+    };
+  });
+
+  return rates;
+}
+
+export default function useExchangeRates(): UseQueryResult<IExchangeRate[]> {
   return useQuery('exchangeRates', () =>
     fetch('/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
       .then((res) => res.text())
-      .then((data) => {
-        console.log(data);
-
-        return data;
-      })
+      .then((text) => parseResponse(text))
   );
 }
