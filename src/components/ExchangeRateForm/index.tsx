6a228236a4c3d9363f3a1608d6e3957575873505
artmadiar <artmadiar@gmail.com>
2022-03-31 22:25:14 +0200

    implement Exchange Rate Form;

--- src/components/ExchangeRateForm/index.tsx
+++ src/components/ExchangeRateForm/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { IExchangeRate } from '../../types';
 
 interface ExchangeRateFormProps {
@@ -5,10 +6,34 @@ interface ExchangeRateFormProps {
   isFetching: boolean;
 }
 
-export default function ExchangeRateForm(props: ExchangeRateFormProps) {
+export default function ExchangeRateForm({ rates }: ExchangeRateFormProps) {
+  const [currencyCode, setCurrencyCode] = useState('');
+  const [sourceAmount, setSourceAmout] = useState(0);
+  const [targetAmount, setTargetAmount] = useState(0);
+
+  function calculateTargetAmount() {
+    if (!rates || !currencyCode) {
+      return setTargetAmount(0);
+    }
+
+    const rate = rates.find((r) => r.code === currencyCode);
+    const amount = rate ? Math.ceil(sourceAmount * rate.amount / rate.rate) : 0;
+
+    setTargetAmount(amount);
+  }
+
   return (
     <div>
-      Exchange Rate Form
+      <input value={sourceAmount} onChange={(e) => setSourceAmout(Number.parseInt(e.target.value, 10) || 0)} />
+      <span> CZK in </span>
+      <input list="currencyList" value={currencyCode} onChange={(e) => setCurrencyCode(e.target.value)} />
+      <datalist id="currencyList">
+        {rates && rates.map((rate, i) => <option key={i} value={rate.code} />)}
+      </datalist>
+
+      <button onClick={(e) => calculateTargetAmount()} >Calculate</button>
+
+      {<p>{targetAmount !== 0 ? `${targetAmount} ${currencyCode}` : '-'}</p>}
     </div>
   );
 }
